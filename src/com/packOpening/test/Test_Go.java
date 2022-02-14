213bf45360a9fa9af4dac00b5da47235e6dd615d
SnugBear <1504535008@qq.com>
2022-02-14 17:24:13 +0800

    IDEA third commit in master

--- src/com/packOpening/test/Test_Go.java
+++ src/com/packOpening/test/Test_Go.java
@@ -2,7 +2,7 @@ package com.packOpening.test;
 
 public class Test_Go {
     public static void main(String[] args) {
-        System.out.println("人品检测，现在开始");
+        System.out.println("人品检测，现在开始~~");
         BOP.Go();
     }
 }
